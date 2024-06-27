0e37fc412bc7a4ff597b97ac72a37ae0d43cdc39
João Silvestre <joao.silvestre@outlook.com>
2024-06-27 22:30:18 +0100

    Fixing Authz Exec message identifier

--- src/CosmosNetwork/Modules/Authz/MessageExecute.cs
+++ src/CosmosNetwork/Modules/Authz/MessageExecute.cs
@@ -7,7 +7,7 @@ namespace CosmosNetwork.Modules.Authz
         CosmosAddress Grantee,
         Message[] Messages) : Message
     {
-        public const string COSMOS_DESCRIPTOR = "/cosmos.authz.v1beta1.MsgExecAuthorized";
+        public const string COSMOS_DESCRIPTOR = "/cosmos.authz.v1beta1.MsgExec";
 
         public override SerializerMessage ToSerialization()
         {
